52375c00739134e1640ec101a3305399a64860f8
Adz1q <mikolajs2211@gmail.com>
2024-10-09 20:57:31 +0200

    app

---
+++ src/main/java/org/adz1q/nextnote/NextNoteApplication.java
@@ -0,0 +1,13 @@
+package org.adz1q.nextnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/adz1q/nextnote/NextNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adz1q.nextnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
